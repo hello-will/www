2a3bd21b4c74ca6d4a2154289662160045d3f5c5
will <895276773@qq.com>
2018-12-09 23:23:06 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,6 @@
 
 sed -i 's/{DOMAIN_NAME}/'"${DOMAIN_NAME}"'/' /etc/nginx/sites-available/default
 chown -R www-data.www-data /var/www/html
-chown -R mysql.mysql /var/lib/mysql
 
 service mysql start
 service php7.0-fpm start
