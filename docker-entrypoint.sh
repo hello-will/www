a043eb3107945d83eff0183a3a93772d26f81fa3
will <895276773@qq.com>
2018-12-09 22:59:55 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 sed -i 's/{DOMAIN_NAME}/'"${DOMAIN_NAME}"'/' /etc/nginx/sites-available/default
+chown -R www-data.www-data /var/www/html
+chown -R mysql.mysql /var/lib/mysql
+
 service mysql start
 service php7.0-fpm start
 service nginx start
+
 tail -f /var/log/nginx/error.log
