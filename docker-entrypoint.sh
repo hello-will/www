bba6127d00b158a83c4b5c8ec0e842324e242248
root <root@host.iounion.top>
2018-12-09 22:40:53 +0800

    www

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 sed -i 's/{DOMAIN_NAME}/'"${DOMAIN_NAME}"'/' /etc/nginx/sites-available/default
-chown -R www-data.www-data /var/www/html
-chown -R mysql.mysql /var/lib/mysql
 service mysql start
 service php7.0-fpm start
 service nginx start
