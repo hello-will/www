04ba827bc3796bf42d7c4e82af95b062d8036fe5
will <895276773@qq.com>
2018-12-09 23:30:59 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,8 +3,12 @@
 sed -i 's/{DOMAIN_NAME}/'"${DOMAIN_NAME}"'/' /etc/nginx/sites-available/default
 chown -R www-data.www-data /var/www/html
 
-service mysql start
-service php7.0-fpm start
-service nginx start
+if [[ ! -d /var/lib/mysql/mysql ]]; then
+  mysql_install_db > /dev/null 2>&1
+fi
+
+service mysql restart
+service php7.0-fpm restart
+service nginx restart
 
 tail -f /var/log/nginx/error.log
