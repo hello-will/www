841e91c6a283fafbb4addbed41ba620c71a628f2
will <895276773@qq.com>
2018-12-10 00:11:13 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,8 +7,8 @@ if [[ ! -d /var/lib/mysql/mysql ]]; then
   mysql_install_db > /dev/null 2>&1
 fi
 
-service mysql restart
-service php7.0-fpm restart
-service nginx restart
+service mysql start
+service php7.0-fpm start
+service nginx start
 
 tail -f /var/log/nginx/error.log
